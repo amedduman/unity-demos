9c04ee83a9761bd8aed0b2afc74877c7ee6a699c
amedduman <amedduman@protonmail.com>
2024-02-17 14:06:21 +0300

    cpg working on tile data editor

--- casual-puzzle-game/Assets/_game/code/Editor/TileDataEditor.cs
+++ casual-puzzle-game/Assets/_game/code/Editor/TileDataEditor.cs
@@ -8,7 +8,7 @@ namespace CasualPuzzle
     {
         SerializedProperty gridValues;
 
-        private void OnEnable()
+        void OnEnable()
         {
             gridValues = serializedObject.FindProperty("gridValues");
         }
@@ -17,75 +17,45 @@ namespace CasualPuzzle
         {
             serializedObject.Update();
 
-            // EditorGUILayout.BeginHorizontal();
-            // EditorGUILayout.PropertyField(gridValues.GetArrayElementAtIndex(0), GUIContent.none, GUILayout.Width(10));
-            // EditorGUILayout.PropertyField(gridValues.GetArrayElementAtIndex(0), GUIContent.none, GUILayout.Width(10));
-            // EditorGUILayout.EndHorizontal();
-            //
-            // return;
+            int columns = 6;
+            int rows = 8;
+            // int index = 0;
+            // for (int y = 0; y < height; y++)
+            // {
+            //     EditorGUILayout.BeginHorizontal();
+            //     for (int x = 0; x < width; x++)
+            //     {
+            //         EditorGUILayout.PropertyField(gridValues.GetArrayElementAtIndex(index), GUIContent.none, GUILayout.Width(40));
+            //         index++;
+            //     }
+            //     EditorGUILayout.EndHorizontal();
+            // }
+            
+            
+            EditorGUI.BeginChangeCheck();
 
-            // EditorGUILayout.BeginHorizontal();
-
-            int width = 6; // Number of elements in each row
-            int height = 8;
-            for (int x = 0; x < width; x++)
+            for (int i = 0; i < rows; i++)
             {
                 EditorGUILayout.BeginHorizontal();
-                for (int y = 0; y < height; y++)
+
+                for (int j = columns - 1; j >= 0; j--)
                 {
-                    EditorGUILayout.PropertyField(gridValues.GetArrayElementAtIndex(x), GUIContent.none, GUILayout.Width(40));
-                    // if (y == height - 1)
-                    // {
-                    //     EditorGUILayout.BeginHorizontal();
-                    //     EditorGUILayout.EndHorizontal();  
-                    // }
+                    // Display each element of the 2D array
+                    EditorGUI.PropertyField(GUILayoutUtility.GetRect(18, 18), gridValues.GetArrayElementAtIndex(
+                        (columns * rows - 1) - (i * columns + j)), GUIContent.none);
                 }
+
                 EditorGUILayout.EndHorizontal();
             }
 
-            // for (int i = 0; i < gridValues.arraySize; i++)
-            // {
-            //     if (i % columnSize == 0 && i != 0)
-            //     {
-            //         EditorGUILayout.BeginVertical();
-            //         EditorGUILayout.EndVertical();
-            //     }
-            //     // if (i % rowSize == 0 && i != 0)
-            //     // {
-            //     //     EditorGUILayout.BeginHorizontal();
-            //     //     EditorGUILayout.EndHorizontal();
-            //     // }
-            //
-            //     EditorGUILayout.PropertyField(gridValues.GetArrayElementAtIndex(i), GUIContent.none, GUILayout.Width(40));
-            // }
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
 
-            // EditorGUILayout.EndHorizontal();
-
+            
             serializedObject.ApplyModifiedProperties();
         }
-
-        // public override void OnInspectorGUI()
-        // {
-        //     serializedObject.Update();
-        //     
-        //     gridValues = serializedObject.FindProperty("gridValues");
-        //
-        //     EditorGUILayout.BeginHorizontal();
-        //
-        //     for (int i = 0; i < gridValues.arraySize; i++)
-        //     {
-        //         // if ((i + 1) % 4 == 0) // Break to the next line after every 4 elements
-        //         //     EditorGUILayout.BeginVertical();
-        //         
-        //         EditorGUILayout.PropertyField(gridValues.GetArrayElementAtIndex(i), GUIContent.none, GUILayout.Width(40));
-        //         if ((i + 1) % 4 == 0) // Break to the next line after every 4 elements
-        //             EditorGUILayout.EndVertical();
-        //     }
-        //
-        //     EditorGUILayout.EndHorizontal();
-        //
-        //     serializedObject.ApplyModifiedProperties();
-        // }
     }
 
 }
