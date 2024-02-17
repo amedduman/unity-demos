fe3b039ce3a402fedaa5e0da46f59b2b50e9718a
amedduman <amedduman@protonmail.com>
2024-02-17 13:34:19 +0300

    working on tile data inspector

--- casual-puzzle-game/Assets/_game/code/data/TileData.cs
+++ casual-puzzle-game/Assets/_game/code/data/TileData.cs
@@ -6,12 +6,28 @@ namespace CasualPuzzle
     [CreateAssetMenu(menuName = "_game/data/tile data")]
     public class TileData : ScriptableObject
     {
+        public int[] gridValues = new int[8];
         public List<Tile> tiles { get; private set; } = new List<Tile>();
 
         // since this is a scriptable object we need to reset its fields
         public void ResetTileData()
         {
             tiles.Clear();
+        }
+        
+        [ContextMenu("fill array")]
+        void fillArray()
+        {
+            gridValues = new int[48];
+            for (int i = 0; i < gridValues.Length; i++)
+            {
+                gridValues[i] = i;
+            }
+            
+            for (int i = 0; i < gridValues.Length; i++)
+            {
+                UnityEngine.Debug.Log(gridValues[i]);
+            }
         }
 
         public void SetTileData(GridHandler gridHandler, Tile tile)
