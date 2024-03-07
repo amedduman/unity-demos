4fbebcb51bc101520624c988d4bdf98775de4b34
amedduman <amedduman@protonmail.com>
2024-03-07 22:34:53 +0300

    find intersect point between two lines

--- math-for-graphics/Assets/LineTest.cs
+++ math-for-graphics/Assets/LineTest.cs
@@ -3,31 +3,42 @@ using UnityEngine;
 
 public class LineTest : MonoBehaviour
 {
-    [SerializeField] float x1;
-    [SerializeField] float x2;
-
-    Vector3 pointA;
-    Vector3 pointB;
+    [SerializeField] float xA;
+    [SerializeField] float xB;
 
     void OnDrawGizmos()
     {
         MarkCenter();
 
-        float y1 = GetY(x1);
-        float y2 = GetY(x2);
+        float x = 0;
+        float y1Cross = 2 * x + 7;
+        float y2Cross = .6f * x + 3;
+        
+        DrawPoint(new Vector3(-2.8571f,1.28571f,0));
+        
+        DrawLine(xA, xB, 2, 7);
+        DrawLine(xA, xB, 0.6666667F, 3);
+    }
 
-        pointA = new Vector3(x1, y1, 0);
-        pointB = new Vector3(x2, y2, 0);
-
+    void DrawLine(float x1, float x2, float m, float b)
+    {
+        float y1 = GetY(x1, m, b);
+        float y2 = GetY(x2, m, b);
+        var pointA = new Vector3(x1, y1, 0);
+        var pointB = new Vector3(x2, y2, 0);
+        
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(pointA, 1f);
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(pointB, 1f);
         
         Debug.DrawLine(pointA, pointB);
+        Gizmos.color = Color.white;
+    }
 
-        var testPoint1 = new Vector3(-2, 1, 0);
-        if (IsPointOnLine(testPoint1))
+    void DrawPoint(Vector3 point, float m, float k)
+    {
+        if (IsPointOnLine(point, m, k))
         {
             Gizmos.color = Color.green;
         }
@@ -35,28 +46,25 @@ public class LineTest : MonoBehaviour
         {
             Gizmos.color = Color.red;
         }
-        Gizmos.DrawWireSphere(testPoint1, 1f);
-
-        var testPoint2 = new Vector3(3, 5, 0);
-        if (IsPointOnLine(testPoint2))
-        {
-            Gizmos.color = Color.green;
-        }
-        else
-        {
-            Gizmos.color = Color.red;
-        }
-        Gizmos.DrawWireSphere(testPoint2, 1f);
+        Gizmos.DrawWireSphere(point, 1f);
+        Gizmos.color = Color.white;
+    }
+    
+    void DrawPoint(Vector3 point)
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(point, 1f);
+        Gizmos.color = Color.white;
     }
 
-    bool IsPointOnLine(Vector3 point)
+    bool IsPointOnLine(Vector3 point, float m, float k)
     {
-        return IsApproximatelyEqual(point.y, GetY(point.x));
+        return IsApproximatelyEqual(point.y, GetY(point.x, m, k));
     }
 
-    float GetY(float x)
+    float GetY(float x, float m, float b)
     {
-        return 0.6666667F * x + 3;
+        return m * x + b;
     }
 
     bool IsApproximatelyEqual(float a, float b, float epsilon = 0.001f)
