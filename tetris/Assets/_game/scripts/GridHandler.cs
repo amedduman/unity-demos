346f5fc478fab9e3265adc97a4c7b698bc389c68
amedduman <amedduman@protonmail.com>
2024-03-12 18:00:00 +0300

    clean up grid code

--- tetris/Assets/_game/scripts/GridHandler.cs
+++ tetris/Assets/_game/scripts/GridHandler.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Tetris
@@ -11,60 +7,54 @@ namespace Tetris
     {
         [SerializeField] Grid grid;
         [SerializeField] Tile tilePrefab;
-        [SerializeField] int rows, columns = 5;
+        [SerializeField] int width = 5;
+        [SerializeField] int height = 5;
         [SerializeField] Vector2 buffer;
-
-        // Vector3 cellSize;
-        readonly List<Tile> tiles = new List<Tile>();
-
 
         public void OnStart(CameraController cam)
         {
-            // cellSize = grid.cellSize;
             GenerateTiles();
             SetCam(cam);
         }
         
         void GenerateTiles()
         {
-            for (int r = 0; r < rows; r++)
+            for (int y = 0; y < height; y++)
             {
-                for (int c = 0; c < columns; c++)
+                for (int x = 0; x < width; x++)
                 {
-                    Vector3Int cellPos = new Vector3Int(c, r, 0);
+                    Vector3Int cellPos = new Vector3Int(x, y, 0);
                     
                     var tileWorldPos = grid.GetCellCenterWorld(cellPos);
                     var tile = Instantiate(tilePrefab, tileWorldPos, Quaternion.identity, transform);
                     
-                    tile.gameObject.name = $"{c}, {r}";
+                    tile.gameObject.name = $"{x}, {y}";
                     tile.cellPos = cellPos;
-                    
-                    tiles.Add(tile);
                 }
             }
         }
 
-        Bounds bo;
         void SetCam(CameraController cam)
         {
-            Bounds bounds = new Bounds();
-            
-            float width = (columns * tiles[0].spriteRenderer.bounds.size.x);
-            float height = (rows * tiles[0].spriteRenderer.bounds.size.y);
-            Vector3 gridLocalCenter = new Vector3(width/2, height/2, 0);
+            Bounds gridBounds = new Bounds();
 
-            bounds.size = new Vector3
+            Bounds tileBounds = tilePrefab.spriteRenderer.bounds;
+            float gridWidth = (width * tileBounds.size.x);
+            float gridHeight = (height * tileBounds.size.y);
+            Vector3 gridLocalCenter = new Vector3(gridWidth/2, gridHeight/2, 0);
+
+            gridBounds.size = new Vector3
             (
-                width, 
-                height,
+                gridWidth, 
+                gridHeight,
                 0
             );
-            bounds.center = transform.position + gridLocalCenter;
+            gridBounds.center = transform.position + gridLocalCenter;
             
-            Bounds boundsBeforeBuffer = bounds;
-            bounds.Expand(new Vector3(buffer.x, buffer.y, 0));
+            Bounds gridBoundsBeforeBuffer = gridBounds;
+            gridBounds.Expand(new Vector3(buffer.x, buffer.y, 0));
             
-            cam.SetPositionAndOrthographicSize(bounds, boundsBeforeBuffer);
+            cam.SetPositionAndOrthographicSize(gridBounds, gridBoundsBeforeBuffer);
         }
     }
 }
